3aa663b38ead628843fdf355316ab732c6e89697
Walter Rafelsberger <walter@elastic.co>
2018-12-27 13:01:14 +0100

    Fixes 6.6 issues.

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-export default kibana => new kibana.Plugin({
-  id: 'kibana-milestones-vis',
-  require: ['elasticsearch'],
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+    name: 'kibana-milestones-vis',
+    uiExports: {
+      visTypes: ['plugins/kibana-milestones-vis/milestones_vis']
+    },
 
-  uiExports: {
-    visTypes: ['plugins/kibana-milestones-vis/milestones_vis']
-  },
-
-  config(Joi) {
-    return Joi.object({
-      enabled: Joi.boolean().default(true),
-    }).default();
-  },
-
-});
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+  });
+}
